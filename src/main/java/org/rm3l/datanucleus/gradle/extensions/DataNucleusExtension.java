4d5aed6488ad58ee5754c59871d8aef24bc40471
Armel S <armel@rm3l.org>
2018-11-11 00:24:10 +0100

    Update Copyright / Licensing headers

--- src/main/java/org/rm3l/datanucleus/gradle/extensions/DataNucleusExtension.java
+++ src/main/java/org/rm3l/datanucleus/gradle/extensions/DataNucleusExtension.java
@@ -1,7 +1,30 @@
+//MIT License
+//
+//Copyright (c) 2018 Armel Soro
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 package org.rm3l.datanucleus.gradle.extensions;
 
 import groovy.lang.Closure;
 import org.gradle.api.Project;
+import org.gradle.api.UnknownTaskException;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskContainer;
@@ -12,6 +35,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Extension for the 'datanucleus' DSL entrypoint
+ */
+@SuppressWarnings("unused")
 public class DataNucleusExtension {
 
     private static final String ENHANCE_TASK_NAME = "enhance";
@@ -21,13 +48,11 @@ public class DataNucleusExtension {
     /**
      * The source sets that hold persistent model.  Default is project.sourceSets.main
      */
+    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private List<SourceSet> sourceSets;
 
     private final SourceSet mainSourceSet;
 
-    /**
-     * Configuration for bytecode enhancement.  Private; see instead {@link #enhance(groovy.lang.Closure)}
-     */
     private final EnhanceExtension enhance;
 
     public DataNucleusExtension(Project project) {
@@ -52,11 +77,11 @@ public class DataNucleusExtension {
         sourceSets.add(sourceSet);
     }
 
+    //Auto-bind the DSL to a Gradle task
     private void enhance(Closure closure) {
         ConfigureUtil.configure(closure, enhance);
 
         final TaskContainer projectTasks = project.getTasks();
-
         projectTasks.create(ENHANCE_TASK_NAME, EnhanceTask.class,
                 task -> {
                     task.getPersistenceUnitName().set(enhance.getPersistenceUnitName());
@@ -76,6 +101,11 @@ public class DataNucleusExtension {
                     task.getIgnoreMetaDataForMissingClasses()
                             .set(enhance.isIgnoreMetaDataForMissingClasses());
                 });
-        projectTasks.getByName("classes").dependsOn(ENHANCE_TASK_NAME);
+        try {
+            projectTasks.getByName("classes").dependsOn(ENHANCE_TASK_NAME);
+        } catch (final UnknownTaskException ute) {
+            //No worries
+            project.getLogger().warn(ute.getMessage(), ute);
+        }
     }
 }
--- src/main/java/org/rm3l/datanucleus/gradle/tasks/EnhanceTask.java
+++ src/main/java/org/rm3l/datanucleus/gradle/tasks/EnhanceTask.java
@@ -1,3 +1,25 @@
+//MIT License
+//
+//Copyright (c) 2018 Armel Soro
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
 package org.rm3l.datanucleus.gradle.tasks;
 
 import org.datanucleus.enhancer.DataNucleusEnhancer;
@@ -15,6 +37,9 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.stream.Stream;
 
+/**
+ * Actual enhancer task
+ */
 public class EnhanceTask extends DefaultTask {
 
     private final Property<String> persistenceUnitName;
@@ -143,7 +168,7 @@ public class EnhanceTask extends DefaultTask {
         final DataNucleusEnhancer enhancer = new DataNucleusEnhancer(api.get().name(), null)
                 .setVerbose(verbose.get())
                 .setClassLoader(new URLClassLoader(classloaderUrls, Thread.currentThread().getContextClassLoader()))
-                .addPersistenceUnit(persistenceUnitName.get()) //TODO Support class list
+                .addPersistenceUnit(persistenceUnitName.get())
                 .setDetachListener(detachListener.get())
                 .setGenerateConstructor(generateConstructor.get())
                 .setGeneratePK(generatePK.get())
