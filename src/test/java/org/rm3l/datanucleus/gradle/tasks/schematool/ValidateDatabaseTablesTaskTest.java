e0578baf511ab0a9dec537df246a53a1bc406c87
Armel S <armel@rm3l.org>
2018-12-24 00:44:53 +0100

    [#2] Add 'validateDatabaseTables' test for the 'skip' DSL configuration

--- src/test/java/org/rm3l/datanucleus/gradle/tasks/schematool/ValidateDatabaseTablesTaskTest.java
+++ src/test/java/org/rm3l/datanucleus/gradle/tasks/schematool/ValidateDatabaseTablesTaskTest.java
@@ -70,4 +70,42 @@ class ValidateDatabaseTablesTaskTest {
         assertTrue(output.contains("DataNucleus SchemaTool : Validation of the schema for classes"));
         assertTrue(output.contains("DataNucleus SchemaTool completed successfully"));
     }
+
+    @Test
+    @DisplayName("should skip validating the database tables against an in-memory datastore")
+    void test_ValidateDBTables_skip(@DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
+        final Path buildGradle = tempDir.resolve("build.gradle");
+        Files.write(buildGradle,
+                ("plugins { id 'org.rm3l.datanucleus-gradle-plugin' }\n\n" +
+                        "repositories {\n" +
+                        "  mavenCentral()\n" +
+                        "}\n" +
+                        "\n" +
+                        "dependencies {\n" +
+                        "  compile 'org.datanucleus:datanucleus-accessplatform-jpa-rdbms:" + DN_JPA_RDBMS_VERSION + "'\n" +
+                        "  compile 'com.h2database:h2:" + H2_VERSION + "'\n" +
+                        "  testCompile 'junit:junit:" + JUNIT_VERSION + "'\n" +
+                        "}\n" +
+                        "\n" +
+                        "datanucleus {\n" +
+                        "  schemaTool {\n" +
+                        "    skip true\n" +
+                        "    api 'JPA'\n" +
+                        "    persistenceUnitName 'myPersistenceUnit'\n" +
+                        "  }\n" +
+                        "}\n")
+                        .getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+
+        //This does not make the build fail. Instead, a stacktrace is output by DataNucleus Enhancer
+        BuildResult result = gradle(tempDir, "build", "validateDatabaseTables", "--debug");
+        assertNotNull(result);
+        BuildTask validateDatabaseTablesTask = result.task(":validateDatabaseTables");
+        assertNotNull(validateDatabaseTablesTask);
+        assertSame(SUCCESS, validateDatabaseTablesTask.getOutcome());
+        String output = result.getOutput();
+        assertNotNull(output);
+        assertTrue(output.contains("SchemaTool Task Execution skipped as requested"));
+    }
 }
