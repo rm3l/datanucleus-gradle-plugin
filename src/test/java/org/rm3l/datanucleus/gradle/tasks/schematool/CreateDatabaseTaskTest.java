33c9611bad6ffb8076cbf5808c1ab6bca944e3f2
Armel S <armel@rm3l.org>
2018-12-24 00:00:23 +0100

    [#2] Enrich 'createDatabase' tests

--- src/test/java/org/rm3l/datanucleus/gradle/tasks/schematool/CreateDatabaseTaskTest.java
+++ src/test/java/org/rm3l/datanucleus/gradle/tasks/schematool/CreateDatabaseTaskTest.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
+import static org.gradle.testkit.runner.TaskOutcome.FAILED;
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.rm3l.datanucleus.gradle.utils.TestUtils.*;
@@ -36,7 +37,7 @@ class CreateDatabaseTaskTest {
 
     @Test
     @DisplayName("should succeed creating the database against an in-memory datastore")
-    void test_schematool_CreateDatabase_does_succeed(@DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
+    void test_CreateDatabase_does_succeed(@DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
         final Path buildGradle = tempDir.resolve("build.gradle");
         Files.write(buildGradle,
                 ("plugins { id 'org.rm3l.datanucleus-gradle-plugin' }\n\n" +
@@ -71,5 +72,128 @@ class CreateDatabaseTaskTest {
         String output = result.getOutput();
         assertNotNull(output);
         assertTrue(output.contains("DataNucleus SchemaTool : Creation of the database (cat=myCatalogName, sch=mySchemaName)"));
+        assertTrue(output.contains("DataNucleus SchemaTool completed successfully"));
+    }
+
+    @Test
+    @DisplayName("should not succeed creating the database against an in-memory datastore, if schema or catalog are missing")
+    void test_CreateDatabase_does_not_succeed_if_no_sch_and_cat(
+            @DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
+        final Path buildGradle = tempDir.resolve("build.gradle");
+        Files.write(buildGradle,
+                ("plugins { id 'org.rm3l.datanucleus-gradle-plugin' }\n\n" +
+                        "repositories {\n" +
+                        "  mavenCentral()\n" +
+                        "}\n" +
+                        "\n" +
+                        "dependencies {\n" +
+                        "  compile 'org.datanucleus:datanucleus-accessplatform-jpa-rdbms:" + DN_JPA_RDBMS_VERSION + "'\n" +
+                        "  compile 'com.h2database:h2:" + H2_VERSION + "'\n" +
+                        "  testCompile 'junit:junit:" + JUNIT_VERSION + "'\n" +
+                        "}\n" +
+                        "\n" +
+                        "datanucleus {\n" +
+                        "  schemaTool {\n" +
+                        "    api 'JPA'\n" +
+                        "    persistenceUnitName 'myPersistenceUnit'\n" +
+                        "  }\n" +
+                        "}\n")
+                        .getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+
+        //This does not make the build fail. Instead, a stacktrace is output by DataNucleus Enhancer
+        BuildResult result = gradle(tempDir, false, "build", "createDatabase");
+        assertNotNull(result);
+        BuildTask createDatabaseTask = result.task(":createDatabase");
+        assertNotNull(createDatabaseTask);
+        assertSame(FAILED, createDatabaseTask.getOutcome());
+        String output = result.getOutput();
+        assertNotNull(output);
+        assertTrue(output.contains("Missing option: schemaName") || output.contains("Missing option: catalogName"));
+        assertFalse(output.contains("DataNucleus SchemaTool : Creation of the database (cat=myCatalogName, sch=mySchemaName)"));
+        assertFalse(output.contains("DataNucleus SchemaTool completed successfully"));
+    }
+
+    @Test
+    @DisplayName("should not succeed creating the database against an in-memory datastore, if schema is missing")
+    void test_CreateDatabase_does_not_succeed_if_no_sch(
+            @DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
+        final Path buildGradle = tempDir.resolve("build.gradle");
+        Files.write(buildGradle,
+                ("plugins { id 'org.rm3l.datanucleus-gradle-plugin' }\n\n" +
+                        "repositories {\n" +
+                        "  mavenCentral()\n" +
+                        "}\n" +
+                        "\n" +
+                        "dependencies {\n" +
+                        "  compile 'org.datanucleus:datanucleus-accessplatform-jpa-rdbms:" + DN_JPA_RDBMS_VERSION + "'\n" +
+                        "  compile 'com.h2database:h2:" + H2_VERSION + "'\n" +
+                        "  testCompile 'junit:junit:" + JUNIT_VERSION + "'\n" +
+                        "}\n" +
+                        "\n" +
+                        "datanucleus {\n" +
+                        "  schemaTool {\n" +
+                        "    api 'JPA'\n" +
+                        "    persistenceUnitName 'myPersistenceUnit'\n" +
+                        "    catalogName 'myCatalogName'\n" +
+                        "  }\n" +
+                        "}\n")
+                        .getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+
+        //This does not make the build fail. Instead, a stacktrace is output by DataNucleus Enhancer
+        BuildResult result = gradle(tempDir, false, "build", "createDatabase");
+        assertNotNull(result);
+        BuildTask createDatabaseTask = result.task(":createDatabase");
+        assertNotNull(createDatabaseTask);
+        assertSame(FAILED, createDatabaseTask.getOutcome());
+        String output = result.getOutput();
+        assertNotNull(output);
+        assertTrue(output.contains("Missing option: schemaName"));
+        assertFalse(output.contains("DataNucleus SchemaTool : Creation of the database (cat=myCatalogName, sch=mySchemaName)"));
+        assertFalse(output.contains("DataNucleus SchemaTool completed successfully"));
+    }
+
+    @Test
+    @DisplayName("should not succeed creating the database against an in-memory datastore, if catalog is missing")
+    void test_CreateDatabase_does_not_succeed_if_no_cat(
+            @DataNucleusPluginTestExtension.TempDir Path tempDir) throws IOException {
+        final Path buildGradle = tempDir.resolve("build.gradle");
+        Files.write(buildGradle,
+                ("plugins { id 'org.rm3l.datanucleus-gradle-plugin' }\n\n" +
+                        "repositories {\n" +
+                        "  mavenCentral()\n" +
+                        "}\n" +
+                        "\n" +
+                        "dependencies {\n" +
+                        "  compile 'org.datanucleus:datanucleus-accessplatform-jpa-rdbms:" + DN_JPA_RDBMS_VERSION + "'\n" +
+                        "  compile 'com.h2database:h2:" + H2_VERSION + "'\n" +
+                        "  testCompile 'junit:junit:" + JUNIT_VERSION + "'\n" +
+                        "}\n" +
+                        "\n" +
+                        "datanucleus {\n" +
+                        "  schemaTool {\n" +
+                        "    api 'JPA'\n" +
+                        "    persistenceUnitName 'myPersistenceUnit'\n" +
+                        "    schemaName 'mySchemaName'\n" +
+                        "  }\n" +
+                        "}\n")
+                        .getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+
+        //This does not make the build fail. Instead, a stacktrace is output by DataNucleus Enhancer
+        BuildResult result = gradle(tempDir, false, "build", "createDatabase");
+        assertNotNull(result);
+        BuildTask createDatabaseTask = result.task(":createDatabase");
+        assertNotNull(createDatabaseTask);
+        assertSame(FAILED, createDatabaseTask.getOutcome());
+        String output = result.getOutput();
+        assertNotNull(output);
+        assertTrue(output.contains("Missing option: catalogName"));
+        assertFalse(output.contains("DataNucleus SchemaTool : Creation of the database (cat=myCatalogName, sch=mySchemaName)"));
+        assertFalse(output.contains("DataNucleus SchemaTool completed successfully"));
     }
 }
